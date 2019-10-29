34c2b14db390318f09a6e413f72ec5015068bc7a
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-10-29 16:16:40 +0800

    Updates

---
+++ precache-manifest.383f741bbfaeaa76acf67f42e0ab6d02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5594ae7c40f30a7b6ef21eb58ea94115",
+    "url": "/cpx/index.html"
+  },
+  {
+    "revision": "6de9116cbde26a1a1bbe",
+    "url": "/cpx/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e239367b14d7afeac6bf",
+    "url": "/cpx/static/js/2.91e9fb8a.chunk.js"
+  },
+  {
+    "revision": "6de9116cbde26a1a1bbe",
+    "url": "/cpx/static/js/main.c6e24c04.chunk.js"
+  },
+  {
+    "revision": "69945e841446679e9b07",
+    "url": "/cpx/static/js/runtime-main.7ccb55d4.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cpx/static/media/logo.25bf045c.svg"
+  }
+]);
