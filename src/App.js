aac3f94daf2ca14fa5faebee32523868a2a723a2
Andy.L.Jin <Andy.L.Jin@newegg.com>
2019-10-30 10:02:54 +0800

    Fix issue react-router 404 referrence https://github.com/facebook/create-react-app/issues/1765

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Wrapper>
             <Item>
               <Link to="/">Home</Link>
